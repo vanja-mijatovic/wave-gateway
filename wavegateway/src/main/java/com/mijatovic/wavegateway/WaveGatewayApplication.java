4dbc075d38b8da4f58b7a337aba5daf8875ed18a
Vanja Mijatovic <vmijatovic2000@gmail.com>
2023-04-20 20:51:49 +0200

    Added Spring Boot project

---
+++ wavegateway/src/main/java/com/mijatovic/wavegateway/WaveGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mijatovic.wavegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaveGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaveGatewayApplication.class, args);
+	}
+
+}
---
+++ wavegateway/src/test/java/com/mijatovic/wavegateway/WaveGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mijatovic.wavegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaveGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
